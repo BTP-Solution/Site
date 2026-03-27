6eed129ee49fc7445a1cc5fd121f4175cd389c01
BtpAdmin <admin@btpsolution.com>
2026-03-27 18:44:07 +0300

    kjkj

---
+++ open-next.config.ts
@@ -0,0 +1,10 @@
+import type { OpenNextConfig } from "@opennextjs/aws/types/open-next.js";
+
+const config: OpenNextConfig = {
+  default: {},
+  middleware: {
+    external: true,
+  },
+};
+
+export default config;
