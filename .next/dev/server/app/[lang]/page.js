e5476a146f84b2fab5e38ddba501bfe7139aec80
BtpAdmin <admin@btpsolution.com>
2026-03-28 08:16:23 +0300

    ada

--- .next/dev/server/app/[lang]/page.js
+++ .next/dev/server/app/[lang]/page.js
@@ -1,6 +1,6 @@
 var R=require("../../chunks/ssr/[turbopack]_runtime.js")("server/app/[lang]/page.js")
 R.c("server/chunks/ssr/node_modules_3d853ed5._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__0f49135f._.js")
+R.c("server/chunks/ssr/[root-of-the-server]__8602b457._.js")
 R.c("server/chunks/ssr/node_modules_e2d1c5df._.js")
 R.c("server/chunks/ssr/[externals]__e8a2741f._.js")
 R.c("server/chunks/ssr/src_app_5b2047f8._.js")
--- .next/dev/static/chunks/src_app_[lang]_page_tsx_2f3fc2cd._.js
+++ .next/dev/static/chunks/src_app_[lang]_page_tsx_2f3fc2cd._.js
@@ -1,7 +1,7 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/_06e9debe._.js"
+  "static/chunks/_0cf7facd._.js"
 ],
     source: "dynamic"
 });
